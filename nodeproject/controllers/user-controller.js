d33963d1bda9f76675fa80ba1b4438a74a8a1bd8
abs-kawser <abskawser77000@gmail.com>
2023-01-10 12:31:33 +0600

    user not inter twice

---
+++ nodeproject/controllers/user-controller.js
@@ -0,0 +1,117 @@
+const { body, validationResult } = require('express-validator');
+const userModel = require("../Models/userModel");
+const bcrypt = require('bcryptjs');
+
+let user = [{
+    email: "user1@sm.com",
+    username: "user1",
+
+},
+{
+    email: "user2@so.com",
+    username: "user2",
+
+},
+{
+    email: "user3@so.com",
+    username: "user3",
+
+},
+{
+    email: "user4@so.com",
+    username: "user4",
+
+}
+];
+
+// mongooges use  in allUser
+async function allUser(req, res, next) {
+    let dbusers = await userModel.find().exec();
+    res.json(dbusers).status(200);
+}
+
+// register logic 
+async function registerUser(req, res, next) {
+   const errors = validationResult(req);
+    console.log(errors.errors);
+    if (errors.errors.length) {
+        return res.status(422).json({
+            errors: errors.array()
+        });
+
+    } else {
+       
+    }
+    const {
+        email,
+        username,
+        password } = req.body;
+        
+      let hashPassword =await bcrypt.hash(password,10)  
+     let newUser =await new  userModel({
+        email,
+        username,
+        password :hashPassword,
+    }).save();  
+   // await newUser  
+    // user.push({
+    //     email,
+    //     username,
+    //     password
+    // })
+    //res.json(req.body);
+    // status code maintain
+    return res.status(201).json(newUser);
+};
+
+
+// createrUser logic 
+async function createrUser(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    const {
+        email,
+        username,
+        password } = req.body;
+
+    let newUser = new userModel({
+        email,
+        username,
+        password
+    })
+    await newUser.save();
+
+    // user.push({
+    //     email,
+    //     username,
+    //     password
+    // })
+    // status code maintain
+    return res.status(201).json(
+       newUser
+    );
+};
+
+
+// get user
+function getUser(req, res, next) {
+    email = req.params.email;
+    let result = user.find(u => u.email === email);
+    return res.json(result).status(200);
+}
+// delete user
+function deleteUserByemail(req, res, next) {
+    email = req.params.email;
+    let index = user.findIndex(u => u.email === email);
+    user.splice(index, 1)
+    return res.json(user).status(200);
+}
+
+// exporting 
+exports.createrUser = createrUser;
+exports.registerUser = registerUser;
+exports.allUser = allUser;
+exports.getUser = getUser;
+exports.deleteUserByemail = deleteUserByemail;
